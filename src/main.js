e6d79969770f683b2f9ad91e050ff2d77be9c61a
Nine Dragon Labs <ninedragonlabs@gmail.com>
2022-01-03 17:48:51 +0700

    v.1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import vuetify from './plugins/vuetify'
+import './plugins/helper'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
